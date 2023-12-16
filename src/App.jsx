2c6cfca2c6c71960c80cfdf491e06b6df4b2cbb2
funmeen <irfanmuhymeen@gmail.com>
2023-12-17 03:12:16 +0800

    Initial commit for digitalgold/

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src="src/assets/norwintrading.logo.png"></img>
+      </header>
+      <div class="flex-container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>  
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>  
+        <div>7</div>
+        <div>8</div>
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
