9269800d231dd30e2e467bd3638f7e2d5a7c9f0e
funmeen <irfanmuhymeen@gmail.com>
2023-12-18 14:45:51 +0800

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import './App.css'
+import reactImg from './assets/norwintrading.logo.png';
+
+function Header(){
+  return(
+    <header>
+        <img src={reactImg} />
+      </header>
+  );
+}
+
+const barTitle = ['Home', 'About', 'Product', 'Calculator', 'Blog', 'Contact Us', 'Sign In'];
+
+function Navbar(){
+  return(
+    <div class="flex-container">
+        <div>{barTitle[0]}</div>
+        <div>{barTitle[1]}</div>
+        <div>{barTitle[2]}</div>  
+        <div>{barTitle[3]}</div>
+        <div>{barTitle[4]}</div>
+        <div>{barTitle[5]}</div>  
+        <div></div>
+        <div>{barTitle[6]}</div>
+      </div>
+  );
+}
 
 function App() {
   return (
-    <>
-      <header>
-        <img src="src/assets/norwintrading.logo.png"></img>
-      </header>
-      <div class="flex-container">
-        <div>Home</div>
-        <div>About</div>
-        <div>Product</div>  
-        <div>Calculator</div>
-        <div>Blog</div>
-        <div>Contact Us</div>  
-        <div></div>
-        <div>Sign In</div>
-      </div>
-    <main>
-      <h2>Time to get started</h2>
-    </main>
+    <div>
+      <Header />  
+      <Navbar />
+      <main>
+        <h2>Time to get started</h2>
+        
+      </main>
       
-    </>
+    </div>
   )
 }
 
